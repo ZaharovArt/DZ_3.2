5874bc5a469d21673c8680ae62da64b31073a9d1
Захаров Артем <zaharov-art@mail.ru>
2023-06-19 21:23:11 +0300

    1

--- src/zad_9.java
+++ src/zad_9.java
@@ -1,6 +1,15 @@
-public class zad_9 {
-    public static void main (String args){
+import java.util.Scanner;
 
-        
+public class zad_9 {
+    public static void main (String[] args){
+
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("Введите M");
+        int m = scanner.nextInt();
+        System.out.println("Введите N");
+        int n =scanner.nextInt();
+        m=m>>2;
+        System.out.println(Integer.toBinaryString(m));
+
     }
 }
